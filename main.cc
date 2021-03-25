60199771b453bac01c08a7c089873fbfb16cd311
Nikki Dong <dong.nikki@gmail.com>
2021-03-25 16:43:44 -0700

    MmapFile: add seek and read

--- main.cc
+++ main.cc
@@ -1,13 +1,45 @@
 #include "mmap_file.hh"
 #include <iostream>
+#include <fstream>
 
-// usage: ./a.out <file_name>
+// usage: ./a.out <file_name> mmap/fstream
+// A driver for comparing performance between using fstream and MmapFile.
+// Open a file and read one byte at a time, and then copy to user buffer.
 int main(int argc, char** argv) {
-    if (argc <= 1) {
+    if (argc <= 2) {
       std::cout << "invalid number of input arguments" << std::endl;
       return -1;
     }
-    nikki::MmapFile file{argv[1]};
+
+
+    char buff[1];
+    size_t total = 0;
+    
+    auto callback = [&total, &buff](const char c){
+                          buff[0] = c;
+                          total++;
+                    };
+
+    if (std::string(argv[2]) == std::string("mmap")) {
+        nikki::MmapFile file{argv[1]};
+   
+
+        // read all bytes into buffer
+        while(size_t n = file.read(buff, sizeof(buff))) {
+            callback(buff[0]);
+        };
+        std::cout << "mmap read " << total << " bytes";
+    } else if (std::string(argv[2]) == std::string("fstream")) {
+        std::fstream file;
+        file.open(argv[1]);
+
+        std::for_each(std::istream_iterator<char>(file),
+                      std::istream_iterator<char>(),
+                      callback);           
+    } else {
+        std::cout << "unspecified command";
+    }
+
 
     return 0;
 }
--- mmap_file.hh
+++ mmap_file.hh
@@ -1,9 +1,12 @@
+#pragma once
+
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <assert.h>
+#include <algorithm>
 
 namespace nikki {
 // A wrapper class that opens a file using mmap,
@@ -17,8 +20,17 @@ public:
 
         file_size_ = get_file_size(file_name);
 
-        mmap_start_ = ::mmap(NULL, file_size_, PROT_READ, MAP_PRIVATE, fd_, 0);
+        mmap_start_ = static_cast<char*>(::mmap(NULL, 
+                                                file_size_,
+                                                PROT_READ,
+                                                MAP_PRIVATE,
+                                                fd_,
+                                                0));
         assert(mmap_start_ != MAP_FAILED);
+        
+        // set cursor and boundary.
+        cur_ = mmap_start_;
+        end_ = cur_ + file_size_;
     }
 
     // Cleanup when goes out of scope
@@ -29,6 +41,30 @@ public:
         assert(rc == 0);
     }
 
+    // copy bytes to destination and advance cursor.
+    // return bytes copied.
+    size_t read(char* dest, size_t n) {
+        n = std::min(n, static_cast<size_t>(end_ - cur_));
+        ::memcpy(dest, cur_, n);
+        cur_ += n;
+        return n;
+    }
+
+    // seek to a position within the file.
+    // return false if seek out of bounds.
+    bool seek(size_t offset_from_begin) {
+        if (mmap_start_ + offset_from_begin >= end_) {
+            return false;
+        }
+        cur_ = mmap_start_ + offset_from_begin;
+        return true;
+    }
+
+    // return the size of the file.
+    size_t size() const {
+        return file_size_;
+    }
+
 private:
     static size_t get_file_size(const char* filename) {
         struct stat st;
@@ -37,9 +73,14 @@ private:
     }
 
     const char* file_name_;
-    void* mmap_start_ = nullptr;
+    char* mmap_start_ = nullptr;
     int fd_ = -1;
     size_t file_size_ = 0;
+    
+    // current cursor.
+    char* cur_ = nullptr;
+    // end of file.
+    char* end_ = nullptr;
 };
 
 } // namespace nikki
